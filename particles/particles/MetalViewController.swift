33f4077a75499595bd58b1c0855bd625d93eb033
naru-jpn <tus.naru@gmail.com>
2020-07-16 01:41:10 +0900

    Prepare MTKView

---
+++ particles/particles/MetalViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MetalViewController.swift
+//  particles
+//
+//  Created by Naruki Chigira on 2020/07/16.
+//  Copyright © 2020 Naruki Chigira. All rights reserved.
+//
+
+import Combine
+import MetalKit
+import UIKit
+
+final class MetalViewController: UIViewController {
+
+    private var metalView: MTKView {
+        view as! MTKView
+    }
+
+    private var cancellables: [AnyCancellable] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            fatalError("Failed to get device from MTLCreateSystemDefaultDevice().")
+        }
+        metalView.device = device
+
+        // Prevent executing metal commands when application is in background.
+        // Reference: https://developer.apple.com/documentation/metal/preparing_your_metal_app_to_run_in_the_background
+        NotificationCenter.Publisher(center: .default, name: UIApplication.willResignActiveNotification)
+            .sink { [weak self] _ in
+                self?.metalView.isPaused = true
+            }
+            .store(in: &cancellables)
+        NotificationCenter.Publisher(center: .default, name: UIApplication.didBecomeActiveNotification)
+            .sink { [weak self] _ in
+                self?.metalView.isPaused = false
+            }
+            .store(in: &cancellables)
+    }
+}
